221b9fbbf1f703ce90c180940b4468838798a5a8
Pietro <pietroaug806@gmail.com>
2024-11-26 15:57:02 -0300

    page not found finished

--- web/src/pages/notFound/index.jsx
+++ web/src/pages/notFound/index.jsx
@@ -1,11 +1,20 @@
-
+import Cabecalho from "../../components/cabecalho";
+import Menu from "../../components/menu";
+import "./index.scss";
 
 
 function NotFound() {
 
     return(
-        <div>
-            <h1>Not found</h1>
+        <div id="pag-not-found">
+            <Menu />
+            <div id="tela">
+                <Cabecalho />
+                <main>
+                    <h1>Not found</h1>
+                    <img src="/assets/images/logo.svg" alt="logo"/>
+                </main>
+            </div>
         </div>
     )
 }
