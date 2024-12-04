5a6f4761344f0f954dd9fdc6a33294a0bc3495dc
Pietro <pietroaug806@gmail.com>
2024-12-04 17:51:37 -0300

    finalizando page consultar

--- web/src/pages/consultar/index.jsx
+++ web/src/pages/consultar/index.jsx
@@ -11,80 +11,98 @@ function Consultar() {
                 <Cabecalho />
                 <main>
                     <section>
-                        <input type="text" placeholder="Buscar livro por nome"/>
-                        <img src="" alt="icon-search" />
+                        <div>
+                            <input type="text" placeholder="Buscar livro pelo nome"/>
+                            <img src="/assets/images/icon-search.svg" alt="icon-search" />
+                        </div>
                     </section>
                     <table>
-                        <thead>
-                            <th> IDENTIFICAÇÃO </th>
-                            <th> LIVRO </th>
-                            <th> AUTOR </th>
-                            <th> LANÇAMENTO </th>
-                            <th> DISPONÍVEL </th>
-                        </thead>
+                        <div>
+                            <thead>
+                                <th class="tamanhoPequeno id"> IDENTIFICAÇÃO </th>
+                                <th class="tamanhoGrande"> LIVRO </th>
+                                <th class="tamanhoMedio"> AUTOR </th>
+                                <th class="tamanhoPequeno"> LANÇAMENTO </th>
+                                <th class="campo-disponivel"> DISPONÍVEL </th>
+                            </thead>
+                        </div>
                         <tbody>
                             <tr>
-                                <td>#01</td>
-                                <td>Harry Potter e a P...</td>
-                                <td>Erick Simons</td>
-                                <td>04/01/2005</td>
-                                <td>Sim</td>
-                                <img src="" alt="icon-alter" />
-                                <img src="" alt="icon-delete" />
+                                <td class="tamanhoPequeno id">#01</td>
+                                <td class="tamanhoGrande" >Harry Potter e a P...</td>
+                                <td class="tamanhoMedio">Erick Simons</td>
+                                <td class="tamanhoPequeno">04/01/2005</td>
+                                <td class="campo-disponivel">Sim</td>
+                                <div>
+                                    <img src="/assets/images/icon-alter.svg" alt="icon-alter" />
+                                    <img src="/assets/images/icon-delete.svg" alt="icon-delete" />
+                                </div>
                             </tr>
                             <tr>
-                                <td>#02</td>
-                                <td>Tartarugas Ninja</td>
-                                <td>Erick Simons</td>
-                                <td>16/09/2019</td>
-                                <td>Sim</td>
-                                <img src="" alt="icon-alter" />
-                                <img src="" alt="icon-delete" />
+                                <td class="tamanhoPequeno id">#01</td>
+                                <td class="tamanhoGrande" >Harry Potter e a P...</td>
+                                <td class="tamanhoMedio">Erick Simons</td>
+                                <td class="tamanhoPequeno">04/01/2005</td>
+                                <td class="campo-disponivel">Sim</td>
+                                <div>
+                                    <img src="/assets/images/icon-alter.svg" alt="icon-alter" />
+                                    <img src="/assets/images/icon-delete.svg" alt="icon-delete" />
+                                </div>
                             </tr>
                             <tr>
-                                <td>#03</td>
-                                <td>Querido Jonh</td>
-                                <td>Erick Simons</td>
-                                <td>20/10/2015</td>
-                                <td>Sim</td>
-                                <img src="" alt="icon-alter" />
-                                <img src="" alt="icon-delete" />
+                                <td class="tamanhoPequeno id">#01</td>
+                                <td class="tamanhoGrande" >Harry Potter e a P...</td>
+                                <td class="tamanhoMedio">Erick Simons</td>
+                                <td class="tamanhoPequeno">04/01/2005</td>
+                                <td class="campo-disponivel">Sim</td>
+                                <div>
+                                    <img src="/assets/images/icon-alter.svg" alt="icon-alter" />
+                                    <img src="/assets/images/icon-delete.svg" alt="icon-delete" />
+                                </div>
                             </tr>
                             <tr>
-                                <td>#04</td>
-                                <td>Barbie</td>
-                                <td>Erick Simons</td>
-                                <td>04/01/2005</td>
-                                <td>Não</td>
-                                <img src="" alt="icon-alter" />
-                                <img src="" alt="icon-delete" />
+                                <td class="tamanhoPequeno id">#01</td>
+                                <td class="tamanhoGrande" >Harry Potter e a P...</td>
+                                <td class="tamanhoMedio">Erick Simons</td>
+                                <td class="tamanhoPequeno">04/01/2005</td>
+                                <td class="campo-disponivel">Sim</td>
+                                <div>
+                                    <img src="/assets/images/icon-alter.svg" alt="icon-alter" />
+                                    <img src="/assets/images/icon-delete.svg" alt="icon-delete" />
+                                </div>
                             </tr>
                             <tr>
-                                <td>#05</td>
-                                <td>Velozes e Furiosos</td>
-                                <td>Erick Simons</td>
-                                <td>20/06/2003</td>
-                                <td>Sim</td>
-                                <img src="" alt="icon-alter" />
-                                <img src="" alt="icon-delete" />
+                                <td class="tamanhoPequeno id">#01</td>
+                                <td class="tamanhoGrande" >Harry Potter e a P...</td>
+                                <td class="tamanhoMedio">Erick Simons</td>
+                                <td class="tamanhoPequeno">04/01/2005</td>
+                                <td class="campo-disponivel">Sim</td>
+                                <div>
+                                    <img src="/assets/images/icon-alter.svg" alt="icon-alter" />
+                                    <img src="/assets/images/icon-delete.svg" alt="icon-delete" />
+                                </div>
                             </tr>
                             <tr>
-                                <td>#06</td>
-                                <td>Arma de Combate</td>
-                                <td>Erick Simons</td>
-                                <td>04/01/2005</td>
-                                <td>Não</td>
-                                <img src="" alt="icon-alter" />
-                                <img src="" alt="icon-delete" />
+                               <td class="tamanhoPequeno id">#01</td>
+                                <td class="tamanhoGrande" >Harry Potter e a P...</td>
+                                <td class="tamanhoMedio">Erick Simons</td>
+                                <td class="tamanhoPequeno">04/01/2005</td>
+                                <td class="campo-disponivel">Sim</td>
+                                <div>
+                                    <img src="/assets/images/icon-alter.svg" alt="icon-alter" />
+                                    <img src="/assets/images/icon-delete.svg" alt="icon-delete" />
+                                </div>
                             </tr>
                             <tr>
-                                <td>#01</td>
-                                <td>Harry Potter e a P...</td>
-                                <td>Erick Simons</td>
-                                <td>04/01/2005</td>
-                                <td>Sim</td>
-                                <img src="" alt="icon-alter" />
-                                <img src="" alt="icon-delete" />
+                               <td class="tamanhoPequeno id">#01</td>
+                                <td class="tamanhoGrande" >Harry Potter e a P...</td>
+                                <td class="tamanhoMedio">Erick Simons</td>
+                                <td class="tamanhoPequeno">04/01/2005</td>
+                                <td class="campo-disponivel">Sim</td>
+                                <div>
+                                    <img src="/assets/images/icon-alter.svg" alt="icon-alter" />
+                                    <img src="/assets/images/icon-delete.svg" alt="icon-delete" />
+                                </div>
                             </tr>
                         </tbody>
                     </table>
